d7b9648606ae3383b8510b908a6e1576dcb3e728
njorocs <njorocs@gmail.com>
2019-05-20 11:48:26 +0300

    Mapping forms to services. Recent visits look and feel.

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/patient/RecentVisitsFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/patient/RecentVisitsFragmentController.java
@@ -21,11 +21,11 @@ import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
+import javax.xml.ws.Service;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import static org.openmrs.module.kenyaemr.metadata.CommonMetadata._EncounterType.HTS;
 
 /**
  * Visit summary fragment
@@ -81,7 +81,8 @@ public class RecentVisitsFragmentController {
             if (counter > 5)
                 break;
             boolean active = false;
-            List<String> formList = new ArrayList<String>();
+            //List<String> formList = new ArrayList<String>();
+            Set<String> serviceSet = new HashSet<String>();
             if (v.getStopDatetime() == null)
                 active = true;
 
@@ -90,7 +91,13 @@ public class RecentVisitsFragmentController {
                 if (encounter.isVoided()) {
                     continue;
                 }
-                formList.add(encounter.getForm().getName());
+
+                if (getFormService(encounter.getForm().getName())==""){
+                    continue;
+                }
+                else {
+                    serviceSet.add(getFormService(encounter.getForm().getName()));
+                }
             }
 
             StringBuilder visitDetails = new StringBuilder().append(DATE_FORMAT.format(v.getStartDatetime()));
@@ -99,7 +106,7 @@ public class RecentVisitsFragmentController {
 
             visits.add(SimpleObject.create(
                     "visitDate", visitDetails.toString(),
-                    "service", formList,
+                    "service", serviceSet,
                     "active", active
             ));
         }
@@ -108,4 +115,80 @@ public class RecentVisitsFragmentController {
 
     }
 
+    private String getFormService (String form) {
+        String key="";
+        Map<String, List<String>> service = new HashMap<String, List<String>>();
+        List<String> hts = new ArrayList<String>();
+        List<String> tracing = new ArrayList<String>();
+        List<String> triage = new ArrayList<String>();
+        List<String> lab = new ArrayList<String>();
+        List<String> consultation = new ArrayList<String>();
+        List<String> contactListing = new ArrayList<String>();
+        List<String> registration = new ArrayList<String>();
+        List<String> screening = new ArrayList<String>();
+        List<String> pharmacy = new ArrayList<String>();
+        List<String> counselling = new ArrayList<String>();
+
+        hts.add("HTS Initial Form");
+        hts.add("HTS Retest Form");
+        hts.add("Hiv status");
+        triage.add("Triage");
+        tracing.add("Client tracing");
+        lab.add("Lab");
+        consultation.add("Clinical Encounter");
+        consultation.add("Clinical Notes Form");
+        consultation.add("Complaints Form");
+        consultation.add("Diagnosis and Treatment Plan");
+        consultation.add("Adverse Drug Reactions Form");
+        consultation.add("Review of Body Systems Form");
+        consultation.add("Referral and Linkage Form");
+        consultation.add("Progress Note");
+        consultation.add("Chronic Illness Form");
+        consultation.add("Exam and observation");
+        consultation.add("STI Treatment");
+        contactListing.add("Contact Listing Form");
+        registration.add("Registration Form");
+        registration.add("Social Status");
+        registration.add("Enrollment");
+        registration.add("Discontinuation");
+        screening.add("Abscess Screening Form");
+        screening.add("Alcohol Screening Form");
+        screening.add("Cervical Cancer Screening Form");
+        screening.add("Hepatitis B Screening Form");
+        screening.add("Hepatitis C Screening Form");
+        screening.add("Immunization Screening Form");
+        screening.add("Allergies Screning Form");
+        screening.add("Drug Overdose Screening Form");
+        screening.add("Pep Screening Form");
+        screening.add("Prep Screening Form");
+        screening.add("Pregnancy and FP Screening Form");
+        screening.add("Risk Reduction Screening form");
+        screening.add("STI Screening Form");
+        screening.add("Violence Screening Form");
+        pharmacy.add("Current Medication Form");
+        pharmacy.add("Counselling Services Form");
+
+        service.put("HTS",hts);
+        service.put("Triage",triage);
+        service.put("Tracing",tracing);
+        service.put("Consultation",consultation);
+        service.put("Lab",lab);
+        service.put("Contact Listing",contactListing);
+        service.put("Registration",registration);
+        service.put("Screening",screening);
+        service.put("Pharmacy",pharmacy);
+        service.put("Counselling",counselling);
+
+        for (Map.Entry<String, List<String>> entry : service.entrySet()) {
+
+            List<String> values = entry.getValue();
+            for(int i = 0; i < values.size();i++){
+                if (values.get(i).equals(form)) {
+                    key = entry.getKey();
+                    break;
+                }
+            }
+        }
+        return key;
+    }
 }
