48e57e58a79fbb158331d58158115979a11457f5
njorocs <njorocs@gmail.com>
2019-08-01 10:47:51 +0300

    Removed unused import

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/kp/ReceivedPostViolenceSupportDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/kp/ReceivedPostViolenceSupportDataEvaluator.java
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- * <p>
+ *
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
@@ -25,8 +25,8 @@ import java.util.Map;
 /**
  * Evaluates a PersonDataDefinition
  */
-@Handler(supports = ReceivedPostViolenceSupportDataDefinition.class, order = 50)
-public class ReceivedPostViolenceSupportDataEvaluator implements PersonDataEvaluator {
+@Handler(supports= ReceivedPostViolenceSupportDataDefinition.class, order=50)
+public class   ReceivedPostViolenceSupportDataEvaluator implements PersonDataEvaluator {
 
     @Autowired
     private EvaluationService evaluationService;
